c64c2a39452b56352524a4a3ba7785efb4302a96
JasonPowr <japower@redhat.com>
2025-06-04 14:50:23 +0100

    fix: add tests, remove root from dockerfile,  handle server gracefully

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/rs/zerolog/log"
 	"github.com/securesign/policy-controller-operator/cmd/webhook"
@@ -24,8 +29,26 @@ func main() {
 		Handler: mux,
 	}
 
-	log.Info().Msgf("HTTPS server listening on %s", *addr)
-	if err := srv.ListenAndServeTLS(*tlsCert, *tlsKey); err != nil && err != http.ErrServerClosed {
-		log.Fatal().Err(err).Msg("failed to start admission webhook server")
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		log.Info().Msgf("HTTPS server listening on %s", *addr)
+		if err := srv.ListenAndServeTLS(*tlsCert, *tlsKey); err != nil && err != http.ErrServerClosed {
+			log.Fatal().Err(err).Msg("failed to start admission webhook server")
+		}
+		close(idleConnsClosed)
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+	log.Info().Msg("Shutdown signal received, shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error().Err(err).Msg("server forced to shutdown")
 	}
+
+	<-idleConnsClosed
+	log.Info().Msg("Server shutdown complete")
 }
